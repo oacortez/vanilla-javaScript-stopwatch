875491662e39fbec946d8269ce7c854b6919465d
Oscar Cortez <oacortez98@gmail.com>
2021-11-18 20:02:41 -0700

    Add comments and assign vars to selectors for butttons

---
+++ watch.js
@@ -0,0 +1,19 @@
+// Query Selectors:
+var startBtn = document.querySelector('#startBtn');
+var stopBtn = document.querySelector('#stopBtn');
+var resetBtn = document.querySelector('#resetBtn');
+
+
+
+
+// event listeners:
+startBtn.addEventListener('click', startTimer);
+
+
+
+
+// functionality:
+
+function () {
+
+}
